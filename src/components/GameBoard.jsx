08243551f79ad12a1ae0f3d9e29363a3134f262c
Natalia-Havashi <natali.gavashi@gmail.com>
2025-04-30 23:44:52 +0300

    remove redundant useState and disable button after click

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -19,7 +19,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rewIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rewIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
